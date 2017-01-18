314c920a9b144f4f58d86d80ef940d70c75e4bdc
Richard Harrah <topplethenunnery@gmail.com>
2017-01-17 23:25:32 -0500

    initial commit

---
+++ src/main/java/io/github/nunnery/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.github.nunnery;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HallmonitorApplication.class);
+	}
+
+}
---
+++ src/test/java/io/github/nunnery/HallmonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.nunnery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HallmonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
